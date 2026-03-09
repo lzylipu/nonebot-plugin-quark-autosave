5b7fd58d9c7a0136e3fc5a13798d984a2e0f099b
lllbot <91266101+lzylipu@users.noreply.github.com>
2026-03-09 20:03:22 +0800

    Update config.py

--- src/nonebot_plugin_quark_autosave/config.py
+++ src/nonebot_plugin_quark_autosave/config.py
@@ -4,9 +4,12 @@ from pydantic import BaseModel
 
 class Config(BaseModel):
     qas_endpoint: str = "http://127.0.0.1:5005"
-    qas_token: str | None = None
+    qas_token: str = ""
     qas_path_base: str = "夸克自动转存"
 
+    # 新增
+    simple_command: str = "1"
+    simple_save_root: str = "自动"
 
-# 配置加载
-plugin_config: Config = get_plugin_config(Config)
+
+plugin_config = get_plugin_config(Config)
